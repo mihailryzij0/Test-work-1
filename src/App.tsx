c6d2f5dd58394e7efdf71ffa84b153d24dd3f42b
mihailryzij0 <mihailryzij0@gmail.com>
2022-05-20 19:02:26 +0300

    created pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import ListUsers from './pages/ListUsers'
+import PostPage from './pages/PostPage'
+import UserPage from './pages/UserPage'
+import UserPostList from './pages/UserPostList'
 
 export default function App() {
   return (
     <Routes>
-      <Route/>
+      <Route path='/' element={<ListUsers/>}/>
+      <Route path='/post' element={<PostPage/>}/>
+      <Route path='/user' element={<UserPage/>}/>
+      <Route path='/post-list' element={<UserPostList/>}/>
     </Routes>
   )
 }

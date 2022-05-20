6a6b385a8c2b702028a473f8aa9b28f0b154cdb9
mihailryzij0 <mihailryzij0@gmail.com>
2022-05-20 18:42:34 +0300

    start-project

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import "./index.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+
+  document.getElementById("root")
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require("path");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const { NODE_ENV } = process.env;
+module.exports = {
+  entry: resolve(__dirname, "src/index.ts"),
+  output: {
+    filename: "index.js",
+    path: resolve(`${__dirname}/dist`),
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  devtool: NODE_ENV === "production" ? "hidden-source-map" : "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.([jt])s$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(woff\woff2\/eot\ttf\/otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./image/[name][ext]",
+        },
+      },
+    ],
+  },
+
+  mode: NODE_ENV === "production" ? "production" : "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["index"],
+    }),
+    new BrowserSyncPlugin(
+      {
+        host: "localhost",
+        port: 3000,
+        proxy: "http://localhost:9000/",
+      },
+      {
+        reload: false,
+      }
+    ),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  optimization: {
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    client: {
+      logging: "info",
+    },
+  },
+};
